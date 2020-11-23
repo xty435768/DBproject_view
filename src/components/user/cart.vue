0860e65945fa10debe46e2d76cfc269d0baf9986
young_glo <younghs@stu.t.edu.vn>
2020-11-23 16:25:45 +0800

    basic style for selling, orders and cart

--- src/components/user/cart.vue
+++ src/components/user/cart.vue
@@ -1,5 +1,15 @@
 <template>
+  <div>
     <div>
-        cart
+      <el-table :data="comments" style="width:100%">
+        <el-table-column prop="name" label="用户" width="200"></el-table-column>
+        <el-table-column prop="time" label="发布时间" width="200" sortable></el-table-column>
+        <el-table-column prop="comment" label="评论内容"></el-table-column>
+      </el-table>
     </div>
+  </div>
 </template>
+
+<style scoped>
+    
+</style>
--- src/components/user/selling.vue
+++ src/components/user/selling.vue
@@ -1,5 +1,12 @@
 <template>
+  <div>
+    这里显示用户卖出的商品订单跟踪
     <div>
-        这里显示用户卖出的商品订单跟踪
+      <el-table :data="comments" style="width:100%">
+        <el-table-column prop="name" label="用户" width="200"></el-table-column>
+        <el-table-column prop="time" label="发布时间" width="200" sortable></el-table-column>
+        <el-table-column prop="comment" label="评论内容"></el-table-column>
+      </el-table>
     </div>
+  </div>
 </template>
