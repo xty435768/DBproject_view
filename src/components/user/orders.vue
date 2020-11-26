676240df33c070eb392736dbb8c09ec2a5d2c664
young_glo <younghs@stu.t.edu.vn>
2020-11-26 18:17:56 +0800

    almost finish selling

--- src/components/user/orders.vue
+++ src/components/user/orders.vue
@@ -5,58 +5,38 @@
         <!-- <el-table-column type="selection" width="45"> </el-table-column> -->
         <el-table-column type="index" label="#"></el-table-column>
         <el-table-column label="商品名称">
-        <template slot-scope="scope">
-          <div>
-            <router-link
-              target="_blank"
-              :to="{
-                path: '/itemDetail',
-                query: {
-                  id: scope.row.bookID,
-                },
-              }"
-              
-            >
-              {{ orderList[scope.$index].bookName }}
-            </router-link>
-          </div>
-        </template>
-      </el-table-column>
-        <el-table-column
-          label="订单价格"
-          prop="order_price"
-          sortable
-        ></el-table-column>
-        <el-table-column
-          prop="order_time"
-          label="发布时间"
-          sortable
-        ></el-table-column>
+          <template slot-scope="scope">
+            <div>
+              <router-link
+                target="_blank"
+                :to="{
+                  path: '/itemDetail',
+                  query: {
+                    id: scope.row.bookID,
+                  },
+                }"
+              >
+                {{ orderList[scope.$index].bookName }}
+              </router-link>
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column label="订单价格" prop="order_price" sortable></el-table-column>
+        <el-table-column prop="order_time" label="下单时间" sortable></el-table-column>
         <el-table-column label="联系方式" prop="mobile"></el-table-column>
         <el-table-column label="是否收到" prop="if_get_book">
           <template slot-scope="scope">
-            <el-tag type="danger" v-if="scope.row.if_get_book == '已下单'"
-              >未收到</el-tag
-            >
-            <el-tag type="success" v-else>已收货</el-tag>
+            <el-tag type="danger" v-if="scope.row.if_get_book == '已下单'">未发货</el-tag>
+            <el-tag type="primary" v-if="scope.row.if_get_book == '已发货'">已发货</el-tag>
+            <el-tag type="success" v-if="scope.row.if_get_book == '已收货'">已收货</el-tag>
           </template>
         </el-table-column>
         <el-table-column label="操作" width="200">
           <template slot-scope="scope">
-            <el-button
-              size="mini"
-              type="danger"
-              @click="handleDelete(scope.$index, scope.row)"
-              >删除</el-button
-            >
+            <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
 
-            <el-button
-              v-if="scope.row.if_get_book == '已下单'"
-              size="mini"
-              type="success"
-              @click="handleSuccess(scope.$index, scope.row)"
-              >确认到货</el-button
-            >
+            <el-button v-if="scope.row.if_get_book == '已发货'" size="mini" type="success" @click="handleSuccess(scope.$index, scope.row)">确认到货</el-button>
+            <el-button v-if="scope.row.if_get_book == '已下单'" size="mini" type="success" disabled>确认到货</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -65,66 +45,63 @@
 </template>
 
 <script>
-import { getCurrentTime } from "../../assets/constants.js";
+import { getCurrentTime } from '../../assets/constants.js'
 
 export default {
   data() {
     return {
-      orderList: [
-        {
-          bookName: "哈哈哈",
-          order_price: 12,
-          order_time: "2020-07-08",
-          if_get_book: 1,
-          mobile: "125347568",
-        },
-        {
-          bookName: "哈哈哈",
-          order_price: 12,
-          order_time: "2020-07-08",
-          if_get_book: 0,
-          mobile: "125347568",
-        },
-        {
-          bookName: "哈哈哈",
-          order_price: 12,
-          order_time: "2020-07-08",
-          if_get_book: 1,
-          mobile: "125347568",
-        },
-      ],
-    };
+      orderList: [],
+    }
   },
   methods: {
     handleSuccess(index, row) {
-      this.orderList[index].if_get_book = "已收货";
+      this.orderList[index].if_get_book = '已收货'
       this.$axios
-        .post("/transaction/update_status", {
+        .post('/transaction/update_status', {
           transactionID: this.orderList[index].transactionID,
           time: getCurrentTime(),
-          status: "已收货",
-          description: "",
+          status: '已收货',
+          description: '',
         })
         .then((resp) => {
-          console.log("showing orders");
-          console.log(resp.data);
+          console.log('showing orders')
+          console.log(resp.data)
           if (resp && resp.status === 200) {
+          } else {
+            console.log('err')
           }
-          else{
-            console.log("err");
-          }
-        });
+        })
     },
-    handleDelete(index, row) {},
+    handleDelete(index, row) {
+      this.$confirm('确定要删除订单吗?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+      })
+        .then(() => {
+          this.$message({
+            type: 'success',
+            message: '删除成功!',
+          })
+          //this.deleteHelper(index, row)
+          this.orderList.splice(index, 1)
+        })
+        .catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除',
+          })
+        })
+    },
     getOrderList() {
-      this.orderList = [];
+      this.orderList = []
       this.$axios
-        .post("/student/get_transaction", {
-          studentID: window.sessionStorage.getItem("user"),
+        .post('/student/get_transaction', {
+          studentID: window.sessionStorage.getItem('user'),
         })
         .then((resp) => {
-          console.log("showing orders");
-          console.log(resp.data);
+          console.log('showing orders')
+          console.log(resp.data)
           if (resp && resp.status === 200) {
             for (var i = 0; i < resp.data.length; i++) {
               this.orderList.push({
@@ -135,15 +112,15 @@ export default {
                 mobile: resp.data[i].commodity.student.mobile,
                 transactionID: resp.data[i].id,
                 bookID: resp.data[i].commodity.id,
-              });
+              })
             }
           }
-        });
+        })
     },
     handleSelectionChange(val) {},
   },
   created() {
-    this.getOrderList();
+    this.getOrderList()
   },
-};
+}
 </script>
--- src/components/user/selling.vue
+++ src/components/user/selling.vue
@@ -1,12 +1,146 @@
 <template>
   <div>
-    这里显示用户卖出的商品订单跟踪
-    <div>
-      <el-table :data="comments" style="width:100%">
-        <el-table-column prop="name" label="用户" width="200"></el-table-column>
-        <el-table-column prop="time" label="发布时间" width="200" sortable></el-table-column>
-        <el-table-column prop="comment" label="评论内容"></el-table-column>
+    <div width="100%">
+      <el-table :data="soldBooks" @selection-change="handleSelectionChange">
+        <!-- <el-table-column type="selection" width="45"> </el-table-column> -->
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column label="商品名称">
+          <template slot-scope="scope">
+            <div>
+              <router-link
+                target="_blank"
+                :to="{
+                  path: '/itemDetail',
+                  query: {
+                    id: scope.row.bookID,
+                  },
+                }"
+              >
+                {{ soldBooks[scope.$index].bookName }}
+              </router-link>
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column label="订单价格" prop="order_price" sortable></el-table-column>
+        <el-table-column label="买家">
+          <template slot-scope="scope">
+            <div>姓名: {{ soldBooks[scope.$index].buyer_name }}</div>
+            <div>电话: {{ soldBooks[scope.$index].buyer_mobile }}</div>
+          </template>
+        </el-table-column>
+        <el-table-column prop="order_time" label="发布时间" sortable></el-table-column>
+        <el-table-column label="货物状态" prop="if_get_book">
+          <template slot-scope="scope">
+            <el-tag type="danger" v-if="scope.row.if_get_book == '已下单'">未发货</el-tag>
+            <el-tag type="primary" v-if="scope.row.if_get_book == '已发货'">已发货</el-tag>
+            <el-tag type="success" v-if="scope.row.if_get_book == '已收货'">已收货</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="200">
+          <template slot-scope="scope">
+            <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+
+            <el-button size="mini" v-if="scope.row.if_get_book == '已下单'" type="success" @click="handleSend(scope.$index, scope.row)">发货</el-button>
+            <el-button size="mini" v-if="scope.row.if_get_book == '已发货'" disabled type="success" >发货</el-button>
+            
+          </template>
+        </el-table-column>
       </el-table>
     </div>
   </div>
 </template>
+
+<script>
+import { getCurrentTime } from '../../assets/constants.js'
+export default {
+  data() {
+    return {
+      soldBooks: [],
+    }
+  },
+  methods: {
+    handleDelete(index, row) {
+      this.$confirm('确定要删除订单吗?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+      })
+        .then(() => {
+          this.$message({
+            type: 'success',
+            message: '删除成功!',
+          })
+          //this.deleteHelper(index, row)
+          this.soldBooks.splice(index, 1)
+        })
+        .catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除',
+          })
+        })
+    },
+    handleSend(index, row) {
+      this.soldBooks[index].if_get_book = '已发货'
+      this.$axios
+        .post('/transaction/update_status', {
+          transactionID: this.soldBooks[index].transactionID,
+          time: getCurrentTime(),
+          status: '已发货',
+          description: '',
+        })
+        .then((resp) => {
+          console.log('showing orders')
+          console.log(resp.data)
+          if (resp && resp.status === 200) {
+          } else {
+            console.log('err')
+          }
+        })
+    },
+
+    getSoldItems() {
+      this.soldBooks = []
+      this.$axios
+        .post('/student/get_release', {
+          studentID: window.sessionStorage.getItem('user'),
+        })
+        .then((resp) => {
+          //console.log(resp.data)
+          if (resp && resp.status === 200) {
+            for (var i = 0; i < resp.data.length; ++i) {
+              //console.log(resp.data[i].id)
+              this.$axios
+                .post('/commodity/get_transaction', {
+                  commodityID: resp.data[i].id,
+                })
+                .then((resp) => {
+                  // console.log(resp)
+                  if (resp && resp.status === 200) {
+                    if (resp.data.length > 3) {
+                      //console.log(resp.data.length)
+                      var trans_data = eval(resp.data)
+                      console.log(trans_data[0])
+                      this.soldBooks.push({
+                        bookName: trans_data[0].commodity.name,
+                        order_price: trans_data[0].commodity.price,
+                        order_time: trans_data[0].currentStatus.time,
+                        buyer_name: trans_data[0].student.name,
+                        buyer_mobile: trans_data[0].student.mobile,
+                        if_get_book: trans_data[0].currentStatus.status,
+                        transactionID: trans_data[0].id,
+                      })
+                    }
+                  }
+                })
+            }
+          }
+        })
+    },
+  },
+
+  mounted() {
+    this.getSoldItems()
+  },
+}
+</script>
